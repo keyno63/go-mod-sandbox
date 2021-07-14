dd1d7a7a67e10e6770e78ae07036154a7562b0c8
Keyno <kofujiw@gmail.com>
2021-07-14 23:18:23 +0900

    clockmanger mock

--- internal/app/config/time/clock_manager.go
+++ internal/app/config/time/clock_manager.go
@@ -12,3 +12,14 @@ type ClockManagerImpl struct {
 func (cm ClockManagerImpl) Now() int64 {
 	return time.Now().Unix()
 }
+
+type ClockManagerMock struct {
+}
+
+func (cm ClockManagerMock) Now() int64 {
+	return int64(1)
+}
+
+type ClockUser struct {
+	ClockM ClockManager
+}
