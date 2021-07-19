d54ac442b278a5f1b2a40f0281eb6b8d252ec5aa
Keyno <kofujiw@gmail.com>
2021-07-20 02:29:25 +0900

    json data 取得部分

--- docker/postgres/initdb/schema.sql
+++ docker/postgres/initdb/schema.sql
@@ -7,3 +7,11 @@ CREATE TABLE IF NOT EXISTS sandbox_table (
 INSERT INTO sandbox_table VALUES (1, 'taro', 'category1');
 INSERT INTO sandbox_table VALUES (2, 'jiro', 'category1');
 INSERT INTO sandbox_table VALUES (3, 'saburo', 'category2');
+
+CREATE TABLE IF NOT EXISTS sandbox_json_table (
+     id integer NOT NULL,
+     updated_at json NOT NULL
+);
+
+INSERT INTO sandbox_json_table VALUES (1, '{"Nanos": 5353, "Seconds": 234}');
+
--- internal/rdb/postgres.go
+++ internal/rdb/postgres.go
@@ -22,20 +22,47 @@ func (p PostgresHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	queryType := r.FormValue("type")
 	switch strings.ToLower(queryType) {
+	case "json":
+		id, _ := strconv.Atoi(r.FormValue("id"))
+		var dto UserDto
+		err := p.Db.QueryRow("SELECT id, updated_at FROM sandbox_json_table where id = $1", id).
+			Scan(&dto.id, &dto.update)
+		if err != nil {
+			fmt.Errorf("error %s", err.Error())
+			w.Header().Set("Content-Type", "application/json")
+			_, _ = w.Write([]byte("ng"))
+			return
+		}
+
+		var ud UpdateAt
+		_ = json.Unmarshal(dto.update, &ud)
+
+		user := User{
+			id:     dto.id,
+			update: &ud,
+		}
+		fmt.Println(user)
+		fmt.Println(user.update)
+		w.Header().Set("Content-Type", "application/json")
+		_, _ = w.Write([]byte("ok"))
+
 	case "insert":
+		fmt.Println(time.Now())
 		id, _ := strconv.Atoi(r.FormValue("id"))
 		data := Data{
 			Id:       id,
 			Name:     r.FormValue("name"),
 			Category: r.FormValue("c"),
 		}
-		goChan := make(chan sql.Result)
+		//goChan := make(chan sql.Result)
+		goChan := make(chan struct{})
+		//defer close(goChan)
 		p.asyncWriteSandboxTable(data, goChan)
-		defer close(goChan)
 		w.Header().Set("Content-Type", "application/json")
 		_, _ = w.Write([]byte("ok"))
 		// ここで待ってしまうので注意
 		<-goChan
+		fmt.Println(time.Now())
 	case "search":
 		p.search(w)
 	default:
@@ -55,15 +82,36 @@ func (p PostgresHandler) search(w http.ResponseWriter) {
 	_, _ = w.Write(jsonValue)
 }
 
-func (p PostgresHandler) asyncWriteSandboxTable(d Data, channel chan<- sql.Result) {
+func (p PostgresHandler) asyncWriteSandboxTable(d Data, channel chan<- struct{}) {
+	//defer close(channel)
+	//defer hoge2(channel)
+	//if true {
+	//	fmt.Println("nothing to do")
+	//	time.Sleep(10 * time.Second)
+	//	//go hoge(channel)
+	//	return
+	//}
 	go func() {
 		// 非同期の確認のために wait 入れている
 		time.Sleep(10 * time.Second)
-		r, err := p.syncWriteSandboxTable(d)
+		_, err := p.syncWriteSandboxTable(d)
 		if err != nil {
 			fmt.Println("failed syncWriteSandboxTable: ", err)
 		}
-		channel <- *r
+		channel <- struct{}{}
+		close(channel)
+	}()
+}
+
+func hoge(channel chan<- struct{}) {
+	channel <- struct{}{}
+	fmt.Println("empty add channel")
+}
+
+func hoge2(channel chan<- struct{}) {
+	go func() {
+		channel <- struct{}{}
+		fmt.Println("empty add channel")
 	}()
 }
 
@@ -111,4 +159,20 @@ type Data struct {
 
 type Response struct {
 	Data []Data `json:"data"`
+}
+
+type UserDto struct {
+	id int
+	//Update *UpdateAt
+	update json.RawMessage
+}
+
+type User struct {
+	id     int
+	update *UpdateAt
+}
+
+type UpdateAt struct {
+	Nanos   int `json:"Nanos"`
+	Seconds int `json:"Seconds"`
 }
