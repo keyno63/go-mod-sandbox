5b2d5319817a4a409492c26dd237c5825554cd97
Keyno <kofujiw@gmail.com>
2021-07-27 23:11:08 +0900

    fix

--- internal/cassandra/cassandra.go
+++ internal/cassandra/cassandra.go
@@ -11,17 +11,17 @@ type Handler struct {
 }
 
 func (c Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	var id, data, update_time string
+	var id, data, updateTime string
 	// SELECT 文は Scan?
 	if err := c.Session.Query("SELECT * FROM test_table;").
-		Scan(&id, &data, &update_time); err != nil {
+		Scan(&id, &data, &updateTime); err != nil {
 		fmt.Println("failed")
 		if err == gocql.ErrNotFound {
 			fmt.Println("ErrNotFound")
 		}
 		fmt.Println(err.Error())
 	}
-	v := fmt.Sprintf("write ret: id=[%s], data=[%s], time=[%s]", id, data, update_time)
+	v := fmt.Sprintf("write ret: id=[%s], data=[%s], time=[%s]", id, data, updateTime)
 	fmt.Println(v)
 	_, _ = w.Write([]byte(v))
 }
