b74e1f639e3f9146fafe952b776cf66ad5140a05
Keyno <kofujiw@gmail.com>
2021-07-30 23:49:32 +0900

    json_parser impl

---
+++ internal/parser/json_parser.go
@@ -0,0 +1,14 @@
+package parser
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+func isParsableJson(value string) bool {
+	if err := json.Unmarshal([]byte(value), &struct{}{}); err != nil {
+		fmt.Printf("failed to paese json, value = [%s], reason = [%s]\n", value, err.Error())
+		return false
+	}
+	return true
+}
---
+++ internal/parser/json_parser_test.go
@@ -0,0 +1,52 @@
+package parser
+
+import "testing"
+
+func Test_isParsableJson(t *testing.T) {
+	type args struct {
+		value string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "正常なJSON",
+			args: args{
+				value: `{
+  "sample": 1
+}`,
+			},
+			want: true,
+		},
+		{
+			name: "正常なJSON. 空のJSONの場合",
+			args: args{
+				value: `{}`,
+			},
+			want: true,
+		},
+		{
+			name: "異常なJSON、value がない",
+			args: args{
+				value: `{"invalid"}`,
+			},
+			want: false,
+		},
+		{
+			name: "異常なJSON、空文字の場合",
+			args: args{
+				value: "",
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isParsableJson(tt.args.value); got != tt.want {
+				t.Errorf("isParsableJson() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
